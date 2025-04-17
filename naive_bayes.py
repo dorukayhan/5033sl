e4a160f3fbaace272e57f7ee68462eb28c7f0261
dorukayhan <dorukayhan@gmail.com>
2025-04-17 03:40:18 -0500

    training errata and test infra
    predefine features and values to look for in util instead of blindly trusting
    the training set + laplace smoothing + boilerplate for testing the model,
    just need to implement nbeval() properly now

--- naive_bayes.py
+++ naive_bayes.py
@@ -12,36 +12,51 @@ def main(argv: list[str]):
     trainset: pd.DataFrame = pd.concat([X_train_full, y_train_full], axis="columns", join="inner") # match instances w/ labels in one df
     if len(X_train_full) != len(y_train_full) != len(trainset):
         raise AssertionError(f"training dataframes have different lengths? xtf: {len(X_train_full)}, ytf: {len(y_train_full)}, combined: {len(trainset)}")
+    # now we begin
     # figure out label probabilities
     p_label: dict[str, Fraction] = {}
     label_counts: pd.Series = y_train_full.groupby("class").size()
-    for label in label_counts.index:
-        # p_label[label] = int(label_counts[label]) / len(y_train_full)
+    for label in util.labels:
+        # labels are balanced-ish, no need to smooth them
         p_label[label] = Fraction(label_counts[label], label_counts.sum())
     print(f"P(label) for each label: {p_label}")
-    # figure out probabilities of category features given each label
-    # continuous features (cap-diameter, stem-height, stem-width) show up as float64s and should be treated differently from category features
-    category_feats: pd.Index = X_train_full.select_dtypes(exclude=[np.number]).columns
-    continuous_feats: pd.Index = X_train_full.select_dtypes(include=[np.number]).columns
-    p_feat: dict[str, dict[str, dict[str, Fraction | float]]] = {} # p_feat[label][feature][value] is P(feature=value|label). value can't be nan
-    for label in label_counts.index:
-        p_feat[label] = {} # dict[str, dict[str, Fraction]]
+    # figure out features' frequencies per label
+    p_feat: dict[str, dict[str, dict[str, float]]] = {} # p_feat[label][feature][value] is P(feature=value|label). value can't be nan
+    for label in util.labels:
+        p_feat[label] = {} # dict[str, dict[str, float]]
         train_given_label: pd.DataFrame = trainset.loc[trainset["class"] == label]
         # category features - find P by just counting
-        for feature in category_feats:
-            p_feat[label][feature] = {} # dict[str, Fraction]
+        for feature in util.category_feats.keys():
+            p_feat[label][feature] = {} # dict[str, float]
             # count the categories (except nans) and their occurrences
             value_counts: pd.Series = train_given_label.groupby(feature).size()
-            for value in value_counts.index:
-                p_feat[label][feature][value] = Fraction(value_counts[value], value_counts.sum())
+            for value in util.category_feats[feature]:
+                # apply laplace smoothing here
+                # denom is value_counts.sum() instead of len(train_given_label) to ignore missing values
+                p_feat[label][feature][value] = (value_counts.get(value, default=0) + (1/len(util.category_feats[feature]))) / (value_counts.sum() + 1)
                 print(f"P({feature}={value}|{label}) = {p_feat[label][feature][value]}")
+            print(f"{value_counts.sum()} instances w/ {feature}")
         # continuous features - assume a normal distribution to find P
-        for feature in continuous_feats:
+        for feature in util.continuous_feats:
+            # ignore missing values again
             p_feat[label][feature] = { # dict[str, float]
                 "mean": train_given_label[feature].mean(),
-                "stdev": train_given_label[feature].std()
+                "variance": train_given_label[feature].var(),
+                "n": len(train_given_label[feature].dropna())
             }
             print(f"params for P({feature}|{label}): {p_feat[label][feature]}")
+    # "training" done, time to evaluate
+    def nbeval(instance: pd.Series) -> str:
+        import random
+        return "e" if random.random() < 0.5 else "p"
+    y_pred: pd.Series = X_test.apply(nbeval, axis="columns", result_type="reduce")
+    result: pd.DataFrame = y_pred.compare(y_test["class"], keep_shape=True, keep_equal=True)
+    TP: int = len(result.loc[(result["self"] == "e") & (result["other"] == "e")])
+    TN: int = len(result.loc[(result["self"] == "p") & (result["other"] == "p")])
+    FP: int = len(result.loc[(result["self"] == "e") & (result["other"] == "p")])
+    FN: int = len(result.loc[(result["self"] == "p") & (result["other"] == "e")])
+    print(f"{len(result)} evals, {TP} TPs, {TN} TNs, {FP} FPs, {FN} FNs")
+    print(f"accuracy {(TP+TN)/len(result)}, recall/TPR {TP/(TP+FN)}, FPR {FP/(FP+TN)}")
     return p_feat
 
 if __name__ == "__main__":
--- util.py
+++ util.py
@@ -1,8 +1,32 @@
 import pandas as pd
+import math
 import random
 from sklearn.model_selection import train_test_split
 from typing import Optional
 from ucimlrepo import fetch_ucirepo, dotdict
+
+# THESE MUST MATCH https://archive.ics.uci.edu/dataset/848/secondary+mushroom+dataset ADDITIONAL VARIABLE INFORMATION
+category_feats: dict[str, set[str]] = {
+    "cap-shape": set(["b", "c", "x", "f", "s", "p", "o"]),
+    "cap-surface": set(["i", "g", "y", "s", "h", "l", "k", "t", "w", "e"]),
+    "cap-color": set(["n", "b", "g", "r", "p", "u", "e", "w", "y", "l", "o", "k"]),
+    "does-bruise-or-bleed": set(["t", "f"]),
+    "gill-attachment": set(["a", "x", "d", "e", "s", "p", "f"]), # i'm assuming "unknown" is just nan since there's not a single question mark in the dataset
+    "gill-spacing": set(["c", "d", "f"]),
+    "gill-color": set(["n", "b", "g", "r", "p", "u", "e", "w", "y", "l", "o", "k", "f"]),
+    "stem-root": set(["b", "s", "c", "u", "e", "z", "r"]),
+    "stem-surface": set(["i", "g", "y", "s", "h", "l", "k", "t", "w", "e", "f"]),
+    "stem-color": set(["n", "b", "g", "r", "p", "u", "e", "w", "y", "l", "o", "k", "f"]),
+    "veil-type": set(["p", "u"]),
+    "veil-color": set(["n", "b", "g", "r", "p", "u", "e", "w", "y", "l", "o", "k", "f"]),
+    "has-ring": set(["t", "f"]),
+    "ring-type": set(["c", "e", "r", "g", "l", "p", "s", "z", "y", "m", "f"]),
+    "spore-print-color": set(["n", "b", "g", "r", "p", "u", "e", "w", "y", "l", "o", "k"]),
+    "habitat": set(["g", "l", "m", "p", "h", "u", "w", "d"]),
+    "season": set(["s", "u", "a", "w"])
+}
+continuous_feats: set[str] = set(["cap-diameter", "stem-height", "stem-width"])
+labels: set[str] = set(["e", "p"])
 
 def get_dataset() -> dotdict:
     """downloads the dataset from uci ml repo"""
@@ -25,3 +49,9 @@ def data_prep(tts_seed: Optional[int]=None) -> tuple[dotdict, pd.DataFrame, pd.D
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=tts_seed_real)
     X_train_real, X_validate, y_train_real, y_validate = train_test_split(X_train, y_train, test_size=0.25, shuffle=False)
     return dataset, X, y, X_train_real, X_validate, X_test, y_train_real, y_validate, y_test
+
+def gauss(a: float, mean: float, variance: float) -> float:
+    """gaussian probability thing"""
+    pay: float = math.exp(-((a - mean) ** 2) / (2 * variance))
+    payda: float = math.sqrt(2 * math.pi * variance)
+    return pay / payda
